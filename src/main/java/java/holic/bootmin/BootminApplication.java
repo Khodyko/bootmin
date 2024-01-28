0b35f2c1fd745a96853962c146722b2743df60f4
khodyko <I.Khodyko@isida.by>
2024-01-28 14:00:20 +0300

    добавлены сущности

---
+++ src/main/java/java/holic/bootmin/BootminApplication.java
@@ -0,0 +1,13 @@
+package java.holic.bootmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootminApplication.class, args);
+    }
+
+}
