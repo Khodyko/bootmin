4fd12f41114a7b572cc8f3a5fb4f195dd3c74e3b
khodyko <I.Khodyko@isida.by>
2024-02-18 16:48:55 +0300

    changes

--- src/main/java/jaava/holic/bootmin/controller/CarController.java
+++ src/main/java/jaava/holic/bootmin/controller/CarController.java
@@ -4,13 +4,14 @@ import jaava.holic.bootmin.model.Car;
 import jaava.holic.bootmin.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(value = "car")
+@RestController
+@RequestMapping(value = "car")
 public class CarController {
-
 
     CarRepository repository;
 
@@ -18,6 +19,7 @@ public class CarController {
     public CarController(CarRepository repository) {
         this.repository = repository;
     }
+
 
     @GetMapping
     public List<Car> getCars() {
